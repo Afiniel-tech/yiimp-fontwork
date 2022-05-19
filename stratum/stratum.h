111660344f1e95a91dfece5ecd1d304c896f69b6
afiniel <44875524+afiniel@users.noreply.github.com>
2022-05-19 08:26:51 -0700

    Stratum add: 0x10 , cosa , minotaurx

--- stratum/stratum.h
+++ stratum/stratum.h
@@ -235,3 +235,6 @@ void sha256_double_hash_hex(const char *input, char *output, unsigned int len);
 #include "algos/xevan.h"
 #include "algos/yespower/yespower.h"
 #include "algos/zr5.h"
+#include "algos/0x10.h"
+#include "algos/cosa.h"
+#include "algos/minotaurx.h"
